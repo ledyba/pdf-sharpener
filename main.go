6b6112d41377d0432bbb374fcc2aa2fac293f23e
Kaede Fujisaki <kaede@hexe.net>
2023-02-27 00:30:44 +0900

    Grab image!

--- main.go
+++ main.go
@@ -19,14 +19,72 @@ func main() {
 	if rs, err = os.Open(*input); err != nil {
 		log.Fatal(err)
 	}
+	defer rs.Close()
 
 	var ws *os.File
 	if ws, err = os.Create(*output); err != nil {
 		log.Fatal(err)
 	}
+	defer ws.Close()
 
 	config := pdfcpu.NewDefaultConfiguration()
 	ctx, err := api.ReadContext(rs, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = api.ValidateContext(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//for i := 0; i < ctx.PageCount; i++ {
+	//	for _, obj := range ctx.Optimize.ImageObjects {
+	//		buf := bytes.Buffer{}
+	//		img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+	//		err = jpeg.Encode(&buf, img, &jpeg.Options{Quality: 98})
+	//		if err != nil {
+	//			log.Fatal(err)
+	//		}
+	//		sd, _, _, err := pdfcpu.CreateImageStreamDict(ctx.XRefTable, &buf, false, false)
+	//		if err != nil {
+	//			log.Fatal(err)
+	//		}
+	//		obj.ImageDict = sd
+	//	}
+	//}
+	for _, e := range ctx.Table {
+		if d, ok := e.Object.(pdfcpu.Dict); ok {
+			if typ := d.Type(); typ == nil || *typ != "Page" {
+				continue
+			}
+			res := d.DictEntry("Resources")
+			if res == nil {
+				continue
+			}
+			xObj := res.DictEntry("XObject")
+			if xObj == nil {
+				continue
+			}
+			for _, o := range xObj {
+				if ref, ok := o.(pdfcpu.IndirectRef); ok {
+					ent, found := ctx.XRefTable.FindTableEntry(ref.ObjectNumber.Value(), ref.GenerationNumber.Value())
+					if !found {
+						continue
+					}
+					sd, ok := ent.Object.(pdfcpu.StreamDict)
+					if !ok {
+						continue
+					}
+					if !sd.Image() {
+						continue
+					}
+					_ = os.WriteFile("image.jpg", sd.Raw, os.ModePerm)
+					log.Print(sd)
+				}
+			}
+		}
+	}
 
 	if err = api.WriteContext(ctx, ws); err != nil {
 		log.Fatal(err)
